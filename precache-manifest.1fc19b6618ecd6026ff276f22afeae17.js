383cfbe6150c419cbf02afb359d589a9b88aca6d
Filip Kozlik <filip.kozlik.public@gmail.com>
2020-07-18 17:44:52 +0200

    Updates

---
+++ precache-manifest.1fc19b6618ecd6026ff276f22afeae17.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ff08f94c58febb495cc3d56aa57461fb",
+    "url": "/Pozivnica/index.html"
+  },
+  {
+    "revision": "0fd284a63542d06a5b63",
+    "url": "/Pozivnica/static/css/main.69438b7e.chunk.css"
+  },
+  {
+    "revision": "d42d941828477e057b40",
+    "url": "/Pozivnica/static/js/2.bd7a6711.chunk.js"
+  },
+  {
+    "revision": "2c598234c7a24970b5fdf032f602e7bc",
+    "url": "/Pozivnica/static/js/2.bd7a6711.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0fd284a63542d06a5b63",
+    "url": "/Pozivnica/static/js/main.e7935dfd.chunk.js"
+  },
+  {
+    "revision": "1d1491370de9bb1100cf",
+    "url": "/Pozivnica/static/js/runtime-main.da1e768a.js"
+  },
+  {
+    "revision": "4cd16d83620e84581c034540ef8d3ab1",
+    "url": "/Pozivnica/static/media/couple_name_small.4cd16d83.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/Pozivnica/precache-manifest.0a334d9ef077b7a7f90efb9533977314.js"
+  "/Pozivnica/precache-manifest.1fc19b6618ecd6026ff276f22afeae17.js"
 );
 
 self.addEventListener('message', (event) => {
